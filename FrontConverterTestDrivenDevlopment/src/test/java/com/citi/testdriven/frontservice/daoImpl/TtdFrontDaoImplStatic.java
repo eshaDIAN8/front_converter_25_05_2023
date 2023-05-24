1a6af69748b222c291dd3ad599e423ccbcdc228f
eshaDIAN8 <98102805+eshaDIAN8@users.noreply.github.com>
2023-05-25 02:11:13 +0530

    Update TtdFrontDaoImplStatic.java

--- FrontConverterTestDrivenDevlopment/src/test/java/com/citi/testdriven/frontservice/daoImpl/TtdFrontDaoImplStatic.java
+++ FrontConverterTestDrivenDevlopment/src/test/java/com/citi/testdriven/frontservice/daoImpl/TtdFrontDaoImplStatic.java
@@ -1,5 +1,8 @@
 package com.citi.testdriven.frontservice.daoImpl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -7,19 +10,27 @@ import org.springframework.stereotype.Component;
 import com.citi.testdriven.frontservice.dao.TtdFrontDao;
 
 @Component
-@Profile("test")
+
 public class TtdFrontDaoImplStatic  implements TtdFrontDao{
-
-	//hv to create hashmap to create data instd of callng h2 db
 	
-	
-
 	
 	@Override
+	@Bean
+	@Profile("test")
 	public String getFormula(String convertedUnit) {
-		System.out.println("inside daoimplstatic java file");
+		System.out.println("inside daoimpl static java file");
 		
-		return "*100";
+		Map<String,String> map = new HashMap<>();
+		map.put("km-meter","*1000");
+		map.put("meter-km", "/1000");
+		map.put("inch-meter","*0.025");
+		map.put("meter-inch","/0.025");
+		map.put("mm-meter","/1000");
+		map.put("meter-mm","*1000");
+		map.put("cm-meter","/100");
+		map.put("meter-cm","*100");
+		
+		return map.get(convertedUnit);
 	}
 
 }
